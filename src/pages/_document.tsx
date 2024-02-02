75bf89eb98d3c0355e763cbcc70d26d374abc3a6
chanyang <chanyang721@gmail.com>
2024-02-02 14:15:43 +0900

    feat: _app.ts, _documents.ts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+import { JSX } from 'react'
+
+
+
+export default function Document(): JSX.Element {
+
+  return (
+    <Html lang="kr">
+      <Head/>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
+}
