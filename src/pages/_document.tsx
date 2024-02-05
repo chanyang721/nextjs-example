d8091d9d214ffd15bbcf4ebf0e18325685d81ae5
chanyang <chanyang721@gmail.com>
2024-02-05 10:18:10 +0900

    fix: head option

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,17 +4,33 @@ import { JSX } from 'react'
 
 
 export default function Document(): JSX.Element {
+  const Forts = () => (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+    </>
+  )
 
   return (
     <Html lang="kr">
-      <Head/>
+      <Head>
+        <Forts/>
+      </Head>
       <body>
-        <title>Boxer Front</title>
-        <meta name='description' content='Boxer page'/>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+      <title>Boxer Front</title>
+      <meta name="description" content="Boxer page"/>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
         <Main/>
         <NextScript/>
       </body>
